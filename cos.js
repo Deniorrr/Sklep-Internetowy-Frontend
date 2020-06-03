5f1fed5bc7279cd599300a89eaea963980078ad4
Deniorrr <49251639+Deniorrr@users.noreply.github.com>
2020-06-03 10:52:50 +0200

    Add files via upload

---
+++ cos.js
@@ -0,0 +1,7 @@
+function cos() {
+    document.getElementById("lul").style.display = "block";
+    document.getElementById("lul2").style.display = "block";
+    document.getElementById("kontener").classList.add("xdd");
+    document.getElementById("lul").classList.add("lull");
+    document.getElementById("lul2").classList.add("lull");
+}
