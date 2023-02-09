0c989c39594b44227183ce35e781132552f79ea5
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2023-02-09 01:40:33 +0100

    Small corrections

---
+++ scripts/koszykScript.js
@@ -0,0 +1,7 @@
+function displayForm() {
+    document.getElementById("form").style.display = "block";
+    document.getElementById("orderButton").style.display = "block";
+    document.getElementById("kontener").classList.add("newHeight");
+    document.getElementById("form").classList.add("displayForm");
+    document.getElementById("orderButton").classList.add("displayForm");
+}
