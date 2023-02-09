301e7693d0d3d2f8e56c9a2f2f0d26a19502dd64
Denis Poczęty <49251639+Deniorrr@users.noreply.github.com>
2023-02-09 01:06:48 +0100

    Delete cos.js

--- cos.js
+++
@@ -1,7 +0,0 @@
-function cos() {
-    document.getElementById("lul").style.display = "block";
-    document.getElementById("lul2").style.display = "block";
-    document.getElementById("kontener").classList.add("xdd");
-    document.getElementById("lul").classList.add("lull");
-    document.getElementById("lul2").classList.add("lull");
-}
